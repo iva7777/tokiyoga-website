26010f441acf084478952dbccacabe81f0ae5d4e
Iva <60818270+iva7777@users.noreply.github.com>
2025-11-20 17:35:14 +0200

    fix rendering issue

--- app/sessions/book/info/page.tsx
+++ app/sessions/book/info/page.tsx
@@ -1,6 +1,11 @@
 "use client"
 import UserInfoForm from "@/components/user-info-form"
+import { Suspense } from "react"
 
 export default function BookingInfoPage() {
-  return <UserInfoForm />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <UserInfoForm />
+    </Suspense>
+  )
 }
