260a422399a4e701bb0aefa3199275b599919048
Iva <60818270+iva7777@users.noreply.github.com>
2025-11-24 21:35:12 +0200

    remove Analytics module and duplications of @font-face in global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -29,7 +28,6 @@ export default function RootLayout({
     <html lang="bg">
       <body className="antialiased" style={{ fontFamily: "var(--font-sans)" }}>
         {children}
-        <Analytics />
       </body>
     </html>
   )
